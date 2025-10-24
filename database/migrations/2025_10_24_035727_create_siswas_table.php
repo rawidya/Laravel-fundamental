d801fcfb4beba140f1b955ad3dba9cdb830e7583
Parama Widyadhana <paramawidyaputraarif@gmail.com>
2025-10-24 13:07:40 +0700

    migration add table

--- database/migrations/2025_10_24_035727_create_siswas_table.php
+++ database/migrations/2025_10_24_035727_create_siswas_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('siswas', function (Blueprint $table) {
             $table->id();
+            $table->string("nama");
+            $table->string("alamat");
+            $table->date("tanggal_lahir");
+            $table->string("kelas");
+            $table->integer("absen");
             $table->timestamps();
         });
     }
